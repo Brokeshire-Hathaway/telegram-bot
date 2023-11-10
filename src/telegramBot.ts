0098909b2c17116b74a00c94addc1b7373c43693
Tom Daniel <0xtomdaniel@gmail.com>
2023-11-10 15:28:37 -0500

    updated mention filter

--- src/telegramBot.ts
+++ src/telegramBot.ts
@@ -22,7 +22,7 @@ export function startTelegramBot() {
 
     const text = ctx.message?.text;
 
-    const emberMention = "@ember_crypto_bot";
+    const emberMention = "@EmberAIBot";
     if (!text?.toLowerCase().includes(emberMention)) return;
 
     const chatbotBody: ChatbotBody = {
