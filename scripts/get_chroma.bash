7e85c50b35d400a66d4c459e209d1e6ae224b7ba
Tom Daniel <0xtomdaniel@gmail.com>
2023-11-13 23:03:47 -0500

    attempt to fix chroma bug

--- scripts/get_chroma.bash
+++ scripts/get_chroma.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-CHROMA_RELEASE_VERSION=0.4.17
+CHROMA_RELEASE_VERSION=0.4.10
 if [ ! -d "./../chroma" ]; then
     git clone -b release/$CHROMA_RELEASE_VERSION https://github.com/chroma-core/chroma.git ../chroma
 fi
