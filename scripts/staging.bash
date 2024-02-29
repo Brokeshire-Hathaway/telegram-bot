3ba311ac2cb1d7babccc83f014136a13da8e7079
Tom Daniel <0xtomdaniel@gmail.com>
2024-02-28 19:04:05 -0500

    updated docker project names

--- scripts/staging.bash
+++ scripts/staging.bash
@@ -5,4 +5,4 @@ cd "$scriptdir"
 
 #rm -rf ../chroma
 ./get_chroma.bash
-docker compose -p ember-engine-stage -f ../docker-compose.staging.yml up -d
+docker compose -p ember-engine_stage -f ../docker-compose.staging.yml up -d
