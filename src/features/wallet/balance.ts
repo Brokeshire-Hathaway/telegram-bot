45944d7b146879c3883e882f596698821878b9bc
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2024-05-07 17:36:51 -0500

    Rewrite balance to use retry strategies, do not query decimals and native contracts.

--- src/features/wallet/balance.ts
+++ src/features/wallet/balance.ts
@@ -1,74 +1,110 @@
-import { ChainData } from "@0xsquid/sdk";
+import { ChainData, TokenData } from "@0xsquid/sdk";
 import {
   NATIVE_TOKEN,
   getAllChains,
   getTokensOfChain,
+  getViemClient,
 } from "../../common/squidDB.js";
 import { getAccountAddress, getSmartAccountFromChainData } from "./index.js";
-import z from "zod";
+import { BiconomySmartAccountV2 } from "@biconomy/account";
+import { Hex, PublicClient, erc20Abi, formatUnits, getContract } from "viem";
 
-const balanceError = z.object({
-  shortMessage: z.string(),
-  metaMessages: z.array(z.string()),
-});
-async function getAccountBalanceOfChain(userId: string, network: ChainData) {
-  console.log();
-  const tokens = getTokensOfChain(network);
-  const tokenAddressToSymbol = new Map<string, string>();
-  if (tokens.length == 0) return tokenAddressToSymbol;
-  const tokenAddresses = tokens
-    .map((v) => {
-      tokenAddressToSymbol.set(v.address, v.symbol);
-      return v.address as `0x${string}`;
-    })
-    .filter((v) => v != NATIVE_TOKEN);
+const NUMBER_OF_RETRIES = 3;
+async function getAccountBalanceOfTokens(
+  account: BiconomySmartAccountV2,
+  tokens: TokenData[],
+  client: PublicClient,
+): Promise<Map<string, string>> {
+  const accountAddress = await getAccountAddress(account);
   const balances = new Map<string, string>();
-  const account = await getSmartAccountFromChainData(userId, network);
-  console.log(
-    "Address",
-    await getAccountAddress(account),
-    "Chain",
-    network.chainName,
-  );
-  console.log();
-  let balanceOfAccount;
+
   try {
-    balanceOfAccount = await account.getBalances(tokenAddresses);
+    await client.getBytecode({
+      address: accountAddress,
+    });
   } catch (error) {
-    const e = await balanceError.safeParseAsync(error);
-    if (!e.success) {
-      console.error(error);
-      return balances;
-    }
-    console.error(e.data.metaMessages.join("\n"));
-    console.error(e.data.shortMessage);
-    console.error();
     return balances;
   }
-  for (const balance of balanceOfAccount) {
-    if (balance.amount === BigInt(0)) continue;
-    const symbol = tokenAddressToSymbol.get(balance.address);
-    if (!symbol) continue;
-    balances.set(symbol, balance.formattedAmount);
+
+  // Find native balance
+  const nativeToken = tokens.find((v) => v.address === NATIVE_TOKEN);
+  if (!nativeToken) return balances;
+  const nativeBalance = await client.getBalance({ address: accountAddress });
+  if (nativeBalance > BigInt(0))
+    balances.set(
+      nativeToken.symbol,
+      formatUnits(nativeBalance, nativeToken.decimals),
+    );
+
+  // Extract all contracts and tokens
+  const erc20Tokens = tokens.filter((v) => v.address !== NATIVE_TOKEN);
+  const erc20TokenContracts = erc20Tokens.map((v) =>
+    getContract({
+      address: v.address as Hex,
+      abi: erc20Abi,
+      client,
+    }),
+  );
+  const erc20Balances = await Promise.allSettled(
+    erc20TokenContracts.map((v) => v.read.balanceOf([accountAddress])),
+  );
+  for (let i = 0; i < erc20TokenContracts.length; i++) {
+    const token = erc20Tokens[i];
+    const erc20Balance = erc20Balances[i];
+    if (erc20Balance.status === "fulfilled") {
+      if (erc20Balance.value === BigInt(0)) continue;
+
+      balances.set(
+        token.symbol,
+        formatUnits(erc20Balance.value, token.decimals),
+      );
+      continue;
+    }
+
+    let tries = 0;
+    while (tries < NUMBER_OF_RETRIES) {
+      try {
+        const balance = await erc20TokenContracts[i].read.balanceOf([
+          accountAddress,
+        ]);
+        if (balance === BigInt(0)) break;
+        balances.set(token.symbol, formatUnits(balance, token.decimals));
+        break;
+      } catch {
+        tries += 1;
+        await new Promise((resolve) => setTimeout(resolve, 100));
+      }
+    }
   }
+
   return balances;
+}
+
+async function getAccountBalanceOfChain(
+  userId: string,
+  network: ChainData,
+): Promise<[string, Map<string, string>]> {
+  const tokens = getTokensOfChain(network);
+  if (tokens.length == 0) return [network.chainName, new Map()];
+  const account = await getSmartAccountFromChainData(userId, network);
+  return [
+    network.networkName,
+    await getAccountBalanceOfTokens(account, tokens, getViemClient(network)),
+  ];
 }
 
 export async function getAllAccountBalances(userId: string) {
   const chains = getAllChains();
-  const balancePerChain = new Map<string, Map<string, string>>();
-  for (const chain of chains) {
-    const chainBalance = await getAccountBalanceOfChain(userId, chain);
-    if (chainBalance.size === 0) continue;
-    balancePerChain.set(chain.networkName, chainBalance);
-  }
-  return balancePerChain;
+  const chainBalances = await Promise.all(
+    chains.map((v) => getAccountBalanceOfChain(userId, v)),
+  );
+  return new Map(chainBalances.filter((v) => v[1].size > 0));
 }
 
 export function formatBalances(balances: Map<string, Map<string, string>>) {
   const balancesPerChain = [];
   for (const [chain, balancesOfChain] of balances.entries()) {
-    let chainBalance = `*${chain}*`;
+    let chainBalance = `*${chain.replace("-", "\\-")}*`;
     let maxSizeBalance = 0;
     for (const balance of balancesOfChain.values()) {
       if (balance.length > maxSizeBalance) maxSizeBalance = balance.length;
