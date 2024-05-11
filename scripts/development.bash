1cac5d46fb300e8015a88afcd15f8bbcb164f51c
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2024-05-10 22:50:33 -0500

    Remove unecessary files for server.

--- scripts/development.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-scriptdir="$(dirname "$0")"
-cd "$scriptdir"
-
-./get_chroma.bash
-docker compose -p ember-engine_dev -f ../docker-compose.develop.yml stop
-docker compose -p ember-engine_dev -f ../docker-compose.develop.yml up
--- scripts/staging.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-scriptdir="$(dirname "$0")"
-cd "$scriptdir"
-
-#rm -rf ../chroma
-./get_chroma.bash
-docker compose -p ember-engine_stage -f ../docker-compose.staging.yml stop
-docker compose -p ember-engine_stage -f ../docker-compose.staging.yml up -d --build
