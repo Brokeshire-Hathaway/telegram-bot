d52d8c83fa6014c1eb76977d59de806a2c957e13
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2024-05-16 14:31:50 -0500

    Properly used port in environment.

--- src/service.ts
+++ src/service.ts
@@ -3,10 +3,10 @@ import bodyParser from "body-parser";
 import sendRouter from "./features/send/index.js";
 import swapRouter from "./features/swap/index.js";
 import { initSquid } from "./common/squidDB.js";
+import { ENVIRONMENT } from "./common/settings.js";
 
 // Create express app
 const app = express();
-const PORT = 3000;
 const HOST = "0.0.0.0";
 app.use(bodyParser.json());
 
@@ -23,8 +23,10 @@ app.use("/swap", swapRouter);
 
 export function startTransactionService() {
   // Log when starting service
-  app.listen(PORT, async () => {
-    console.log(`Transaction service running at http://${HOST}:${PORT}`);
+  app.listen(ENVIRONMENT.PORT, async () => {
+    console.log(
+      `Transaction service running at http://${HOST}:${ENVIRONMENT.PORT}`,
+    );
     await initSquid();
     return HOST;
   });
