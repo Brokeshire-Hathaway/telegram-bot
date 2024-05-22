2991d5a9dd1a2d313d208414f24a53ed01b51abf
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2024-05-22 12:33:58 -0500

    Avoid unnecessary jsonb column.

--- src/features/frontendApi/common.ts
+++ src/features/frontendApi/common.ts
@@ -8,15 +8,16 @@ export const Route = z.object({
   transaction_id: z.number().int(),
   amount: z.bigint().positive(),
   token: z.string(),
+  token_address: address,
   chain: z.string(),
+  chain_id: z.string(),
   address: z.union([z.literal("OWNER"), address]),
   order: z.number().int(),
 });
-const RouteCreate = Route.pick({
-  amount: true,
-  token: true,
-  chain: true,
-  address: true,
+const RouteCreate = Route.omit({
+  id: true,
+  transaction_id: true,
+  order: true,
 });
 type RouteCreate = z.infer<typeof RouteCreate>;
 
@@ -26,16 +27,14 @@ export const Transaction = z.object({
   id: z.number().int(),
   identifier: z.string().uuid(),
   type: TransactionType,
-  information: z.record(z.string(), z.union([z.string(), z.number()])),
   fees: z.bigint().positive(),
   total: z.bigint().positive(),
   created_at: z.date(),
 });
-const TransactionCreate = Transaction.pick({
-  type: true,
-  information: true,
-  fees: true,
-  total: true,
+const TransactionCreate = Transaction.omit({
+  id: true,
+  identifier: true,
+  created_at: true,
 });
 type TransactionCreate = z.infer<typeof TransactionCreate>;
 
@@ -49,17 +48,25 @@ export async function createTransaction(
     const tx = await dbTransaction.one(sql.type(
       Transaction.pick({ identifier: true, id: true }),
     )`
-        INSERT INTO transaction ("type", information, fees, total)
+        INSERT INTO transaction ("type", fees, total)
         VALUES (
             ${transaction.type},
-            ${sql.jsonb(transaction.information)},
             ${transaction.fees},
             ${transaction.total}
         )
         RETURNING id
     `);
     await dbTransaction.query(sql.typeAlias("void")`
-        INSERT INTO route (amount, token, "order", transaction_id, chain, address)
+        INSERT INTO route (
+          amount,
+          token,
+          "order",
+          transaction_id,
+          chain,
+          address,
+          token_address,
+          chain_id
+        )
         SELECT *
         FROM ${sql.unnest(
           routes.map((v, i) => [
@@ -69,8 +76,19 @@ export async function createTransaction(
             tx.id,
             v.chain,
             v.address,
+            v.token_address,
+            v.chain_id,
           ]),
-          ["int8", "varchar", "int8", "int8", "varchar", "varchar"],
+          [
+            "int8",
+            "varchar",
+            "int8",
+            "int8",
+            "varchar",
+            "varchar",
+            "varchar",
+            "varchar",
+          ],
         )}
     `);
     return tx.identifier;
