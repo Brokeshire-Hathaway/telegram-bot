a941a65d31b0765df7aacf80fc6ce5216cf6f52c
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2024-05-27 10:36:53 -0500

    Remove unnecessary files.

--- scripts/production.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-scriptdir="$(dirname "$0")"
-cd "$scriptdir"
-
-#rm -rf ../chroma
-./get_chroma.bash
-docker compose -p ember-engine_prod -f ../docker-compose.production.yml stop
-docker compose -p ember-engine_prod -f ../docker-compose.production.yml up -d
