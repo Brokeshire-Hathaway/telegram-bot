96a69b08d7c50ca1699c3143281debdee15061f8
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2024-06-03 14:15:44 -0500

    Create waitlist for users that do not have access

--- src/features/user/index.ts
+++ src/features/user/index.ts
@@ -7,3 +7,12 @@ export async function isUserWhitelisted(userId: number): Promise<boolean> {
     WHERE user_id = ${BigInt(userId)}
   `);
 }
+
+export async function addUserToWaitList(userId: number, username: string) {
+  const pool = await getPool();
+  return await pool.query(sql.typeAlias("void")`
+    INSERT INTO user_waitlist (user_id, username)
+    VALUES (${BigInt(userId)}, ${username})
+    ON CONFLICT (user_id) DO NOTHING
+  `);
+}
