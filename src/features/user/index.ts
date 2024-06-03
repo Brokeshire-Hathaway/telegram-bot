34e88fc81bc458479412e08fbf3b37838263d5de
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2024-06-03 15:05:04 -0500

    Create referral code command for admin users.

--- src/features/user/index.ts
+++ src/features/user/index.ts
@@ -1,4 +1,12 @@
 import { getPool, sql } from "../../common/database";
+import z from "zod";
+
+const User = z.object({
+  id: z.number(),
+  user_id: z.bigint(),
+  username: z.string(),
+  is_admin: z.boolean(),
+});
 
 export async function isUserWhitelisted(userId: number): Promise<boolean> {
   const pool = await getPool();
@@ -14,5 +22,13 @@ export async function addUserToWaitList(userId: number, username: string) {
     INSERT INTO user_waitlist (user_id, username)
     VALUES (${BigInt(userId)}, ${username})
     ON CONFLICT (user_id) DO NOTHING
+  `);
+}
+
+export async function isUserAdmin(userId: number) {
+  const pool = await getPool();
+  return await pool.exists(sql.type(User.pick({ is_admin: true }))`
+    SELECT is_admin FROM user_whitelist
+    WHERE user_id = ${BigInt(userId)} and is_admin
   `);
 }
