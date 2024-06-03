c4823b6cc8df67cc23ffc0e46421ddb8c030c8f1
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2024-06-03 15:39:41 -0500

    Add code redemption command for joining ember.

--- src/features/user/codes.ts
+++ src/features/user/codes.ts
@@ -1,5 +1,6 @@
 import z from "zod";
 import { getPool, sql } from "../../common/database";
+import { isUserWhitelisted } from ".";
 
 const Code = z.object({
   id: z.number(),
@@ -19,4 +20,34 @@ export async function createReferralCode(
         VALUES (${numberOfUses}, ${username})
         RETURNING identifier, code
     `);
+}
+
+export async function redeemCode(
+  code: string,
+  userId: number,
+  username: string,
+): Promise<"redeemed" | "failed"> {
+  const pool = await getPool();
+
+  if (await isUserWhitelisted(userId)) return "failed";
+
+  const id = BigInt(userId);
+  return await pool.transaction(async (transaction) => {
+    const accessCode = await transaction.maybeOne(sql.typeAlias("id")`
+        UPDATE access_code
+        SET remaining_uses = GREATEST(remaining_uses - 1, 0)
+        WHERE code = ${code} and remaining_uses > 0
+        RETURNING id
+    `);
+    if (!accessCode) return "failed";
+    await transaction.query(sql.typeAlias("void")`
+        DELETE FROM user_waitlist
+        WHERE user_id = ${id};
+    `);
+    await transaction.query(sql.typeAlias("void")`
+        INSERT INTO user_whitelist (user_id, username, access_code_id)
+        VALUES (${id}, ${username}, ${accessCode.id})
+    `);
+    return "redeemed";
+  });
 }
