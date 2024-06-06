97d9c3066dee6440fa7c6cb20a66b5734020a61f
Juan Sebastián Cárdenas Rodríguez <jcardenasr123@gmail.com>
2024-06-06 11:45:38 -0500

    Correct table order definition.

--- db/schemas/4.sql
+++ db/schemas/4.sql
@@ -22,6 +22,30 @@ CREATE TABLE fund_code (
 CREATE TRIGGER fund_code_updated_at
 BEFORE UPDATE ON fund_code FOR each ROW EXECUTE PROCEDURE trigger_update_timestamp();
 
+CREATE TABLE access_code (
+  id SERIAL PRIMARY KEY,
+  identifier uuid UNIQUE NOT NULL DEFAULT gen_random_uuid(),
+  code VARCHAR(32) UNIQUE NOT NULL DEFAULT substring(md5(random()::text), 1, 9),
+  remaining_uses INT NOT NULL,
+  created_by VARCHAR NOT NULL,
+  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+  updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+CREATE TRIGGER access_code_updated_at
+BEFORE UPDATE ON access_code FOR each ROW EXECUTE PROCEDURE trigger_update_timestamp();
+CREATE UNIQUE INDEX access_code_idx ON access_code (
+    code, remaining_uses
+);
+
+CREATE TABLE "user" (
+  id SERIAL PRIMARY KEY,
+  access_code_id INT REFERENCES access_code(id),
+  telegram_id BIGINT UNIQUE NOT NULL,
+  username VARCHAR NOT NULL,
+  is_admin BOOLEAN NOT NULL DEFAULT false,
+  joined_at TIMESTAMPTZ NOT NULL DEFAULT now()
+);
+
 CREATE TABLE "transaction" (
   id SERIAL PRIMARY KEY,
   identifier uuid UNIQUE NOT NULL DEFAULT gen_random_uuid(),
@@ -50,30 +74,6 @@ CREATE TABLE route (
 );
 CREATE UNIQUE INDEX route_idx ON route (
     transaction_id, "order"
-);
-
-CREATE TABLE access_code (
-  id SERIAL PRIMARY KEY,
-  identifier uuid UNIQUE NOT NULL DEFAULT gen_random_uuid(),
-  code VARCHAR(32) UNIQUE NOT NULL DEFAULT substring(md5(random()::text), 1, 9),
-  remaining_uses INT NOT NULL,
-  created_by VARCHAR NOT NULL,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-  updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
-);
-CREATE TRIGGER access_code_updated_at
-BEFORE UPDATE ON access_code FOR each ROW EXECUTE PROCEDURE trigger_update_timestamp();
-CREATE UNIQUE INDEX access_code_idx ON access_code (
-    code, remaining_uses
-);
-
-CREATE TABLE "user" (
-  id SERIAL PRIMARY KEY,
-  access_code_id INT REFERENCES access_code(id),
-  telegram_id BIGINT UNIQUE NOT NULL,
-  username VARCHAR NOT NULL,
-  is_admin BOOLEAN NOT NULL DEFAULT false,
-  joined_at TIMESTAMPTZ NOT NULL DEFAULT now()
 );
 
 CREATE TABLE "user_address" (
