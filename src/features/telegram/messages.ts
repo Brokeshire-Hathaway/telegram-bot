b391aa6e84206275a6ab223ddd4c42c8a9f79a8a
Tom Daniel <0xtomdaniel@gmail.com>
2024-06-19 14:26:35 -0400

    updated messages

--- src/features/telegram/messages.ts
+++ src/features/telegram/messages.ts
@@ -1,14 +1,14 @@
-export const START_MESSAGE = `Hey there, crypto enthusiast! 🔥 I'm Ember, your charismatic companion in the vast universe of crypto and DeFi! 😎🚀 Whether you're a seasoned trader or just starting out, I'm here to light up your journey.
+export const START_MESSAGE = `Hey there, crypto enthusiast! 🔥 I'm Ember, your personal crypto companion. 😎🚀 Whether you're a seasoned trader or just starting out, I'm here to light up your journey through the DeFi universe. 🌌
 
 Curious about what Ember can do for you? Try the following messages:
 
-1️⃣ Give me information on arb
+1️⃣ \`Swap arb for op\`
 
-2️⃣ I want to buy 5 matic in polygon
+2️⃣ \`Send 0.01 eth to Jack\`
 
-3️⃣ Send 0.01 eth to Jack
+3️⃣ \`Price of Arweave\`
 
-Use \`/help\` to learn more ways you can use Ember.`;
+Use \`/help\` to discover even more things that Ember can do. 💁‍♂️`;
 
 export const SUCCESS_FUND_MESSAGE = (
   url: string,
@@ -18,17 +18,21 @@ export const SUCCESS_FUND_MESSAGE = (
 
 export const DEFAULT_EMBER_MESSAGE = `Hello there! 🌟
 
-Got an invite code? 🎉 Simply use the \`/join\` command followed by your code to unlock the full Ember experience.
+Got an invite code? 🎉 Simply send \`/join <invite code>\` with your code to unlock the full Ember experience.
+
+Example: \`/join 12345abcd\`
 
 If you're not yet part of the Ember crew, don't worry, you've been added to our waitlist. 📝 We'll reach out as soon as a spot opens up!`;
 
 export const CODE_REDEEMED_SUCCESS = (codes: readonly { code: string }[]) => {
-  const codesMessage = codes.map((v) => `- ${v.code}`).join("\n");
-  return `Congratulations! 🎉 Your exclusive invite code has been successfully redeemed. You now have 5 shiny new invite codes to share with your friends and fellow crypto enthusiasts. 💌
+  const codesMessage = codes.map((v) => `• ${v.code}`).join("\n");
+  return `Congratulations! 🎉 Your exclusive invite code has been successfully redeemed. Welcome to the Ember family.
+
+**Level 1:** You now have 5 shiny new invite codes to share with your friends and fellow crypto enthusiasts. 💌
 
 ${codesMessage}
 
-Spread the warmth and bring more people into our growing family! 🌟 Rewards await, but be sure to only invite individuals who will actively engage with Ember. Inactive users won't contribute to unlocking more invite codes and rewards for you.
+Rewards await, but be sure to only invite people who will actively engage with Ember to increase your points and unlock more invite codes. 🌟
 
 Together, let's fuel the flames of DeFi innovation! 🔐🔗💰`;
 };
